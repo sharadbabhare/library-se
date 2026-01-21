114f05dc55c5f53b029c7f1186b3950bb716c346
Sharad Babhare <sharadbabhare@gmail.com>
2026-01-21 20:53:22 +0530

    Sprint 2: Borrow and return book

--- tests/test_library.py
+++ tests/test_library.py
@@ -2,6 +2,7 @@ import unittest
 from src.library import Library
 
 
+# ---------------- SPRINT 1 TESTS ----------------
 class TestSprint1(unittest.TestCase):
 
     def test_add_book_success(self):
@@ -14,6 +15,28 @@ class TestSprint1(unittest.TestCase):
         library.add_book("B1", "Python Programming", "Guido van Rossum")
         with self.assertRaises(ValueError):
             library.add_book("B1", "Java Programming", "James Gosling")
+
+
+# ---------------- SPRINT 2 TESTS ----------------
+class TestSprint2(unittest.TestCase):
+
+    def setUp(self):
+        self.library = Library()
+        self.library.add_book("B1", "Python Programming", "Guido van Rossum")
+
+    def test_borrow_book(self):
+        self.library.borrow_book("B1")
+        self.assertEqual(self.library.books["B1"]["status"], "Borrowed")
+
+    def test_borrow_already_borrowed_book(self):
+        self.library.borrow_book("B1")
+        with self.assertRaises(ValueError):
+            self.library.borrow_book("B1")
+
+    def test_return_book(self):
+        self.library.borrow_book("B1")
+        self.library.return_book("B1")
+        self.assertEqual(self.library.books["B1"]["status"], "Available")
 
 
 if __name__ == "__main__":
